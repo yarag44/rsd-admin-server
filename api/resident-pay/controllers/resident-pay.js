7d40ef1c3e9be304b0802def69fb02a7191ff0b1
Alberto GAray <yarag44@hotmail.com>
2022-01-10 22:49:41 -0800

    First rsd-admin-server Commit

---
+++ api/resident-pay/controllers/resident-pay.js
@@ -0,0 +1,57 @@
+'use strict';
+const stripe = require("stripe")("sk_test_51JTzmlIUUyTPZHVWFSCtjXUqrrXc3cVQxvwOREgwNiV7IaGGwY3DNreTaSZQd2TYqyjE1MgH9a4Gr1EXQRe8NnHJ00Ux6jAPbt");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+
+    async create(ctx)
+    {
+
+        const { auth,formData,amount,strapitoken,user,comments,month,year } = ctx.request.body;
+
+        console.log(amount);
+        console.log(user.id);
+
+
+        // const charge = await stripe.charges.create({
+        //     amount: (parseFloat(amount).toFixed(2) * 100).toFixed(0), //totalPayment * 100,
+        //     currency: "mxn",
+        //     source: strapitoken,
+        //     description: `ID Usuario: ${user.id}` ,
+            
+        // });
+
+       
+        const createPay = [];
+
+        let data = {
+
+            Amount : amount,
+            Year : year,
+            Month : month,
+            Resident : user.Resident,
+        }
+
+        console.log(user);
+
+        const validData = await strapi.entityValidator.validateEntityCreation(
+            strapi.models.resident_pay,
+            data
+        );
+
+        console.log(validData);
+
+        const entry = await strapi.query("resident-pay").create(validData);
+        createPay.push(entry);
+
+
+        return createPay;
+
+    }
+
+
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17d7c911-425f-497d-adfa-eab2a48f321e'
+};
